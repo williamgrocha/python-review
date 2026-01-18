c755723042e9806699299ac9d2ccf4b788e42cf5
William Graham <williammrochag@gmail.com>
2026-01-18 12:38:52 -0300

    add opcao remover

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
+nomes = []
+
 def menu():
     print("1 - Cadastrar nome")
     print("2 - Listar nomes")
-    print("3 - Sair")
+    print("3 - Remover nome")
+    print("4 - Sair")
 
-nomes = []
+def cadastrar_nome():
+    nome = input("Digite um nome: ")
+    nome = nome.strip()
+    nome = nome.capitalize()
+    if nome in nomes:
+        "Esse nome já foi cadastrado. Tente novamente."
+    elif nome == "":
+        print("Insira um nome válido.")
+    else:
+        nomes.append(nome)
+        print("Nome cadastrado!")
+
+def listar_nomes():
+    if nomes == []:
+        print("Não há nenhum nome cadastrado.")
+    else:
+        print("Nomes cadastrados: ")
+        for n in nomes:
+            print(">", n)
+
+def remover_nome():
+    remover = input("Nome a ser removido: ")
+    remover = remover.strip()
+    remover = remover.capitalize()
+    if remover in nomes:
+        nomes.remove(remover)
+        print(remover, "foi removido com sucesso!")
+    else: 
+        print("Este nome não consta na lista.")
 
 while True:
     menu()
     opcao = input("Escolha uma opção: ")
     if opcao == "1":
-        nome = input("Digite um nome: ")
-        nomes.append(nome)
-        print("Nome cadastrado!")
-
+        cadastrar_nome()
     elif opcao == "2":
-        print("Nomes cadatrados:")
-        for n in nomes:
-            print(">", n)
+        listar_nomes()
 
     elif opcao == "3":
+        remover_nome()
+
+    elif opcao == "4":
         print("Saindo...")
         break
-    
+
     else:
         print("Opção Inválida")
