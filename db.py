eb2583c45793a532be357b99783d7935c891054a
William Graham <williammrochag@gmail.com>
2026-01-21 01:16:25 -0300

    add: adiciona função atualizar_nome

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ def init_db():
     conn = sqlite3.connect("nomes.db")
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS pessoas
-                (id INTEGER PRIMARY KEY, nome TEXT NOT NULL)''')
+                (id INTEGER PRIMARY KEY, nome TEXT NOT NULL UNIQUE)''')
     conn.commit()
     conn.close()
 
@@ -40,3 +40,19 @@ def deletar_nome(nome):
     else:
         conn.close()
         return True
+    
+def alterar_nome(nomeAntigo, novoNome):
+    conn = sqlite3.connect("nomes.db")
+    c = conn.cursor()
+    try:
+        c.execute("UPDATE pessoas SET nome = ? WHERE nome = ?", (novoNome, nomeAntigo))
+        if c.rowcount == 0:
+            conn.close()
+            return False
+        else:
+            conn.commit()
+            conn.close()
+            return True
+    except sqlite3.IntegrityError:
+        conn.close()
+        return 0
--- main.py
+++ main.py
@@ -4,18 +4,16 @@ def menu():
     print("1 - Cadastrar nome")
     print("2 - Listar nomes")
     print("3 - Remover nome")
-    print("4 - Sair")
+    print("4 - Atualizar nome")
+    print("5 - Sair")
 
 def cadastrar_nome():
-    nome = input("Digite um nome: ")
-    nome = nome.strip()
-    nome = nome.capitalize()
-
+    nome = input("Digite um nome: ").strip().capitalize()
     if nome == "":
         print("Insira um nome válido.")
         return
     if not db.inserir_nome(nome):
-        print("Esse nome já consta em nossos cadastros.")
+        print("Esse nome já consta em nossos cadastros. Nada foi alterado.")
     else:
         print("Nome cadastrado com sucesso!")
 
@@ -28,16 +26,33 @@ def listar_nomes():
             print(">", i[0])
     
 def remover_nome():
-    nome = input("Nome a ser removido: ")
-    nome = nome.strip()
-    nome = nome.capitalize()
+    nome = input("Nome a ser removido: ").strip().capitalize()
     if nome == "":
         print("Nome inválido. Tente novamente.")
         return
     if db.deletar_nome(nome):
         print("%s foi removido com sucesso."%nome)
     else:
-        print("Esse nome não consta em nossos registros.")
+        print("Esse nome não consta em nossos registros. Nada foi alterado.")
+
+def atualizar_nome():
+    nome = input("Nome a ser alterado: ").strip().capitalize()
+    if nome == "":
+        print("Insira um nome válido.")
+        return
+    novoNome = input("Novo nome: ").strip().capitalize()
+    if novoNome == "":
+        print("Insira um nome válido.")
+        return
+
+    resultado = db.alterar_nome(nome, novoNome)
+    if resultado == 0:
+        print("O novo nome já consta em nossos registros. Nada foi alterado.")
+    elif resultado:
+        print("Nome atualizado com sucesso!")
+    else:
+        print("O nome a ser alterado não consta em nossos registros. Nada foi alterado.")
+
 
 def main():
     db.init_db()
@@ -51,8 +66,11 @@ def main():
 
         elif opcao == "3":
             remover_nome()
-
+        
         elif opcao == "4":
+            atualizar_nome()
+
+        elif opcao == "5":
             print("Saindo...")
             break
 
